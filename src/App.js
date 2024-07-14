4347b226099b72ba431090b684fa1e1fe8a45a66
Jimmy <jameswafula2002@gmail.com>
2024-07-15 00:21:39 +0300

    coursell

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Router, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterScreen from "./pages/RegisterScreen";
+import CoursesPage from "./pages/CoursesPage";
+import CreateCoursePage from "./pages/CreateCoursePage";
+import ViewCourse from "./pages/ViewCourse";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/courses" element={<CoursesPage />} />
+        <Route path="/newcourse" element={<CreateCoursePage />} />
+        <Route path="/viewcourse/:id" element={<ViewCourse />} />
+      </Routes>
     </div>
   );
 }
